4136696fd3c06acd5b6e86d68e3a975e5f1db558
Collin Cromwell <collcrom@gmail.com>
2017-10-19 17:44:07 -0500

    added dynamic profile pages

--- controllers/user.js
+++ controllers/user.js
@@ -4,10 +4,6 @@ const User = require('../models/user');
 const Beer = require('../models/beer');
 const bcrypt = require('bcrypt');
 
-router.get('/', (req, res)=>{
-	res.send('profile page');
-});
-
 router.route('/register')
 	.get((req,res)=>{
 		res.render('user/register', {loginMessage: ''});
@@ -24,25 +20,36 @@ router.route('/register')
 						res.send(err)
 					req.session.logged = true;
 					req.session.username = user.username;
-					res.redirect('/');
+					res.render('user/profile', {user: user, logged: req.session.logged});
 				})
 			}
 		})
 	})
 
-router.post('/login', (req, res)=>{
-	User.findOne({username: req.body.username}, (err, user)=>{
-		if(err)
-			res.send('error');
-		req.session.logged = true;
-		req.session.username = user.username;
+router.route('/login')
+	.post((req, res)=>{
+		User.findOne({username: req.body.username}, (err, user)=>{
+			if(err)
+				res.send('error');
+			req.session.logged = true;
+			req.session.username = user.username;
+			res.render('user/profile', {user: user, logged: req.session.logged})
+		})
+	})
+
+router.route('/logout')
+	.post((req, res)=>{
+		req.session.destroy()
 		res.redirect('/');
 	})
-})
 
-router.post('/logout', (req, res)=>{
-	req.session.destroy()
-	res.redirect('/');
-})
+router.route('/:id')
+	.get((req, res)=>{
+		User.findById(req.params.id, (err, user)=>{
+			if(err)
+				res.send(err)
+			res.render('user/profile', {user: user, logged: req.session.logged})
+		})
+	})
 
 module.exports = router;
