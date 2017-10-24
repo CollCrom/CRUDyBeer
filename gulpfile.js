ab63fc8153da73c855136103313f14b52c1380d8
Collin Cromwell <collcrom@gmail.com>
2017-10-24 08:58:45 -0500

    fucked up the button colors but I fixed it

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,18 @@
 const gulp = require('gulp');
 const less = require('gulp-less');
 const livereload = require('gulp-livereload');
-const cleanCSS = require('gulp-clean-css');
+//const cleanCSS = require('gulp-clean-css');
 
 gulp.task('less-css', ()=>{
 	gulp.src('./public/styles/*.less')
 		.pipe(less())
-		.pipe(cleanCSS())
+		//.pipe(cleanCSS()) CSS minify
 		.pipe(gulp.dest('./public/styles/'))
 		.pipe(livereload());
 })
 
 gulp.task('watch', () =>{
-	const server = livereload();
+	const server = livereload({ start: true });
 	gulp.watch(['./public/styles/*.less'], ['less-css']);
 })
 
